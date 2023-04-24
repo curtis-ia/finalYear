36c052d4f4751ed77a17dd89a32bd93917589f7b
curtis-ia <67473096+curtis-ia@users.noreply.github.com>
2023-04-24 18:47:14 +0100

    Update SeOutput.jsx

--- src/components/SeOutput.jsx
+++ src/components/SeOutput.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { grossValueChecker, ExpenseValueChecker, pensionValueChecker } from "./SelfEmployed";
-
+import { useRef } from 'react';
 
 
 var profit;
@@ -14,13 +14,25 @@ var net;
 
 
 function SeOutput() {
+        // This ref will be connected to the overlay div
+  const overlayRef = useRef();
+  // This function is called when the "Start Searching" button gets clicked
+  const openSearch = () => {
+    overlayRef.current.style.width = '100%';
+  };
+
+  // This function is called when the "Close" button is clicked
+  const closeSearch = () => {
+    overlayRef.current.style.width = '0%';
+  };
+
 
 
     function profitCalculator() {
         var totalIncome = grossValueChecker()
         var totalExpenses = ExpenseValueChecker()
         profit = totalIncome - totalExpenses;
-        return profit;
+        return profit.toLocaleString();
     }
     function taxableIncomeCalculator() {
         var taxAllowance = 12570;
@@ -36,7 +48,7 @@ function SeOutput() {
         }
 
 
-        return incomeTaxable;
+        return incomeTaxable.toLocaleString();
 
     }
     function IncomeTax() {
@@ -83,7 +95,7 @@ function SeOutput() {
             tax = 0
         }
 
-        return Math.ceil(tax);
+        return Math.ceil(tax).toLocaleString();
 
     }
 
@@ -95,7 +107,7 @@ function SeOutput() {
         else {
             c2Nic = 0
         }
-        return Math.ceil(c2Nic);
+        return Math.ceil(c2Nic).toLocaleString();
     }
 
     function class4Nic() {
@@ -120,20 +132,20 @@ function SeOutput() {
             b = 2.73 / 100 * secondNicPaid;
             c4Nic = a + b;
         }
-        return Math.ceil(c4Nic);
+        return Math.ceil(c4Nic).toLocaleString();
 
     }
 
     function NIC() {
         totalNic = Math.ceil(c2Nic + c4Nic);
-        return totalNic;
+        return totalNic.toLocaleString();
 
     }
     function pensionContribution() {
         var pensionPaid = pensionValueChecker();
 
         pension = pensionPaid;
-        return pension;
+        return pension.toLocaleString();
     }
     function netPay() {
         var a = profit;
@@ -143,27 +155,82 @@ function SeOutput() {
 
         net = a - b - c - d
 
-        return Math.ceil(net);
+        return Math.ceil(net).toLocaleString();
 
     }
     return (
         <div>
+        <div className="resultOutput">
 
-            <h1> Profit: {profitCalculator()} </h1>
-            <p> Taxable Income: {taxableIncomeCalculator()}</p>
-            <p> Tax to pay: {IncomeTax()}</p>
-            <p> Class 2 NIC: {class2Nic()}</p>
-            <p> Class 4 NIC: {class4Nic()}</p>
-            <p> Total NIC contribution: {NIC()}</p>
-            <p> Pension to pay: {pensionContribution()}</p>
-            <p> Net Pay: {netPay()}</p>
+            <h2> Profit: {"£"+profitCalculator()} </h2>
+            <p> Taxable Income: {"£"+taxableIncomeCalculator()}</p>
+            <p> Tax to pay: {"£"+IncomeTax()}</p>
+            <p> Class 2 NIC: {"£"+class2Nic()}</p>
+            <p> Class 4 NIC: {"£"+class4Nic()}</p>
+            <p> Total NIC contribution: {"£"+NIC()}</p>
+            <p> Pension to pay: {"£"+pensionContribution()}</p>
+            <h1> Net Pay: {"£"+netPay()}</h1>
 
-
-
-
+        <p className="disclaimer">Disclaimer: These tax calculations are for illustrative purposes only. No guarantee is made for the accuracy of the data provided. Consult a qualified professional financial advisor before making any financial decisions.</p>
+ <button onClick={openSearch} className='open-button'>
+         Assumption
+        </button>
+    {/* The search overlay */}     
         </div>
-    )
+    <div ref={overlayRef} class='overlay'>
+        <button class='close-button' onClick={closeSearch}>
+          &times;
+        </button>
+        <div class='overlay-content'>
+          <form>
+    <div className= "assumptions">        
+           <p className='search-text'> 
+The calculations are based on the following assumptions:
+</p>
+<p className='search-text'>
+<h2> Umbrella calculation </h2>
+<ol>
+
+<li>1. Figures are based on the current tax year 2023/2024 rates </li>  
+<li>2. This is based on 22 working days  </li>  
+<li>3. No expenses  </li>  
+<li>4. Holiday pay is advanced  </li>  
+<li>5. Contractor Umbrella’s standard margin (£11.25 per week/£45 per month) is included in this calculation  </li>  
+<li>6. These calculations are inclusive of the new NI thresholds for the current year.  </li>  
+<li>7. Income tax is calculated based upon the standard rate of 1250L. If your tax rate differs from this then the income tax calculations may differ from what you actually end up paying in income tax </li> 
+</ol>
+
+<h2> Limited Company calculation </h2>
+<ul>
+<li>1. Figures are based on the current year rates </li>  
+<li>2. Any billable work included within the ‘Annual Revenue’ figure does not fall within the scope of the IR35 rules. </li>
+<li>3. These calculations are inclusive of the new NI thresholds for the current year.  </li>  
+
+</ul>
+
+<h2> SelfEmployed calculation </h2>
+
+<ul>
+<li>1. All numbers are calculated on an annual basis </li> 
+<li>2. The rates used to calculate income tax and national insurance are those for the current tax year 2023/2024. </li> 
+<li>3. Income tax is calculated for UK tax payers excluding those resident in Scotland. </li> 
+<li>4. Scottish income tax rates differ from the rest of the UK and are not accounted for in these calculations </li>
+<li>5. Income tax is calculated based upon the standard rate of 1250L. If your tax rate differs from this then the income tax calculations may differ from what you actually end up paying in income tax </li> 
+<li>6. National Insurance is calculated assuming the tax payer is on Category letter A. If you are under 21, an apprentice under 25 or over the state pension age then the national insurance calculations may differ from what  you actually end up paying in national insurance </li> 
+
+</ul>
+</p>
+            </div>
+          </form>
+        </div>
+      </div>
+</div>
+  )
+    
 }
+
+
+        
 
 export default SeOutput;
 
